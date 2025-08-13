efe1091348f02bb76027d398b0098dc8e97d37d2
Nikolay <siniks7@yandex.ru>
2025-08-13 11:13:44 +0700

    Стилизация компонента

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,22 @@
 <script setup></script>
 
 <template>
-  <button>Кнопка</button>
+   <button class="button">Кнопка</button>
 </template>
+
+<style scoped>
+.button {
+  border: none;
+  border-radius: 10px;
+  background: var(--gradient);
+  padding: 14px;
+  font-family: var(--font);
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--primary);
+  cursor: pointer;
+}
+.button:hover {
+  background: var(--gradient-inverted);
+}
+</style>
