78093cd2a0fb934adf588afca3b15184b8e3cc5a
Nikolay <siniks7@yandex.ru>
2025-08-14 10:52:20 +0700

    Events

--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,10 +1,15 @@
 <script setup>
 import IconLocation from "../icons/IconLocation.vue";
 import Button from "./Button.vue";
+
+function log() {
+  // eslint-disable-next-line no-undef
+  console.log("Log function call");
+}
 </script>
 
 <template>
-  <Button>
+  <Button @click="log()">
     <IconLocation />
     Изменить город
   </Button>
