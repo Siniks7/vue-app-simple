f1cff0ed8ed7296ae2be3b9eae6951b45139cabb
Nikolay <siniks7@yandex.ru>
2025-09-22 15:50:02 +0700

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { ref } from "vue";
 import CitySelect from "./components/CitySelect.vue";
 import Stat from "./components/Stat.vue";
-const data = {
+
+let savedCity = ref("Moscow");
+let data = ref({
   label: "Влажность",
   stat: "90%",
-};
+});
+
+let arr = ref([1]);
+let map = ref(new Map([["1", 1]]));
+
 function getCity(city) {
-  // eslint-disable-next-line no-undef
-  console.log(city);
+  savedCity.value = city;
+  data.value.stat = "20%";
+  arr.value.push(2);
+  map.value.set("2", 2);
 }
 </script>
 
